783591bb24714e39691bd896a49154d265f04990
leonardoCipriani <lbarbosacipriani@gmail.com>
2021-08-15 18:55:26 -0300

    novo arquivo de testes

--- calculadorargb.py
+++ calculadorargb.py
@@ -3,8 +3,10 @@ import cv2
 imagem=cv2.imread("../imagens_artigos/lena_original.png")
 
 
-cv2.imshow('originalk',imagem)
+
 img_gray=cv2.cvtColor(imagem,cv2.COLOR_BGR2GRAY)
+img_gray=cv2.blur(imagem,(7,7))
+cv2.imshow('originalk',img_gray)
 cv2.waitKey()
 
 
